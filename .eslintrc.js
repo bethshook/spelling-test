4d64d379ce53fac747eadd9e9bbe758e0e6a872a
Beth Shook <eshook@wearenolte.com>
2020-08-04 23:11:40 -0500

    Remove TS and install random words library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module"
   },
+  plugins: ["react", "prettier", "react-hooks"],
   extends: [
-    "airbnb-typescript-prettier"
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:prettier/recommended"
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
