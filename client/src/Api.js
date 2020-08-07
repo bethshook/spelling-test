8479496eab5169d73a106e007689986f1c26e438
Beth Shook <beth.shook@gmail.com>
2020-08-07 14:13:38 -0500

    Revert API URI change

--- client/src/Api.js
+++ client/src/Api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const URI = 'http://localhost:8080/api/';
+
 const ApiInstance = axios.create({
   timeout: 10000,
   headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
@@ -7,10 +9,10 @@ const ApiInstance = axios.create({
 
 const API = {
   getWord: () => {
-    return ApiInstance.get(`/api/word`);
+    return ApiInstance.get(`${URI}word`);
   },
   submitWord: (submission) => {
-    return ApiInstance.post(`/api/word`, submission);
+    return ApiInstance.post(`${URI}word`, submission);
   },
 };
 
